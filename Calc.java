e263ea61a3f199b1e0901bb65cdc64779e8897a4
k.beliaev <Tlt-b@ya.ru>
2016-10-27 10:26:24 +0400

    1.2 day

--- Calc.java
+++ Calc.java
@@ -7,72 +7,95 @@ import java.io.InputStreamReader;
 
 public class Calc {
     public static void main(String[] args) throws IOException {
+      //  boolean run = true;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Выберите тип калькулятора: s - обычный, i - инженерный");
         String type = reader.readLine();
-        if (type.equals("s")){
-            System.out.println("Введите первое число или 'i' для перехода в инженерный режим калькулятора");
-            int i = Integer.parseInt(reader.readLine());
-//        while ()
-            System.out.println("Введите знак операции");
-            String op = reader.readLine();
-            System.out.println("Введите второе число");
-            int i1 = Integer.parseInt(reader.readLine());
-            std(i, op, i1);
-        }
+        if (type.equals("s")) {
+            //while (run) {
+                try {
+                    System.out.println("Введите первое число");
+                    int i = Integer.parseInt(reader.readLine());
+                    System.out.println("Введите знак операции");
+                    String op = reader.readLine();
+                    System.out.println("Введите второе число");
+                    int i1 = Integer.parseInt(reader.readLine());
+                    Std.std(i, op, i1);
+                } catch (NumberFormatException e) {
+                    System.out.println("Неверный формат введенных данных");
+                  //  run = false;
+                }
+            }
+        // }
+        else
         if (type.equals("i")){
-            System.out.println("Инженерный тип калькулятора. Вычисляющий: sin,cos,tan,log,log10." +
-                    " \nВведите математическую функцию для вычесления:");
-            String op = reader.readLine();
-            System.out.println("Введите число");
-            int j1 = Integer.parseInt(reader.readLine());
-            inj(op, j1);
-        }
-    }
-
-    public static void std(int j1, String op, int j2) throws IOException {
-        int res = 0;
-        if (op.equals("+")) {
-            res = j1 + j2;
-        }
-        if (op.equals("-")) {
-            res = j1 - j2;
-        }
-        if (op.equals("*")) {
-            res = j1 * j2;
-        }
-        try {
-            if (op.equals("/")) {
-                res = j1 / j2;
+            try {
+                System.out.println("Инженерный тип калькулятора. Вычисляющий: sin,cos,tan,log,log10." +
+                        " \nВведите математическую функцию для вычесления:");
+                String op = reader.readLine();
+                if (op.equals("sin")|op.equals("cos")|op.equals("tan")|op.equals("log")|op.equals("log10")) {
+                    System.out.println("Введите число");
+                    int j1 = Integer.parseInt(reader.readLine());
+                    Inj.inj(op, j1);
+                }
+                else {
+                    System.out.println("Неверно введена функция");
+                    System.exit(0);
+                }
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Неверный формат введенных данных");
             }
         }
-        catch (ArithmeticException e) {
-            System.out.println("Деление на ноль невозможно");
+        else {
+            System.out.println("Выбран неверный тип калькулятора");
             System.exit(0);
         }
-        System.out.println("Результат: " + res);
-        //return res;
     }
-
-    public static void inj(String op, int j1) throws IOException {
-        int res = 0;
-        if (op.equals("sin")) {
-            res = (int) Math.sin(j1);
+    public static class Std {
+        public static void std(int j1, String op, int j2) throws IOException {
+            int res = 0;
+            if (op.equals("+")) {
+                res = j1 + j2;
+            }
+            else
+            if (op.equals("-")) {
+                res = j1 - j2;
+            }
+            else
+            if (op.equals("*")) {
+                res = j1 * j2;
+            }
+            try {
+                if (op.equals("/")) {
+                    res = j1 / j2;
+                }
+            } catch (ArithmeticException e) {
+                System.out.println("Деление на ноль невозможно");
+                System.exit(0);
+            }
+            System.out.println("Результат: " + res);
         }
-        if (op.equals("cos")) {
-            res = (int) Math.cos(j1);
-        }
-        if (op.equals("tan")) {
-            res = (int) Math.tan(j1);
-        }
-        if (op.equals("log")) {
-            res = (int) Math.log(j1);
-        }
-        if (op.equals("log10")) {
-            res = (int) Math.log10(j1);
-        }
-        System.out.println("Результат: " + res);
     }
-
-
+    public static class Inj extends Std {
+        public static void inj(String op, int j1) throws IOException {
+            int res = 0;
+            if (op.equals("sin")) {
+                res = (int) Math.sin(j1);
+            }
+            if (op.equals("cos")) {
+                res = (int) Math.cos(j1);
+            }
+            if (op.equals("tan")) {
+                res = (int) Math.tan(j1);
+            }
+            if (op.equals("log")) {
+                res = (int) Math.log(j1);
+            }
+            if (op.equals("log10")) {
+                res = (int) Math.log10(j1);
+            }
+            System.out.println("Результат: " + res);
+        }
+    }
 }
